2b5590de03f21c5b0f5fe43b382458ad43ac8baf
Nikusion <bilima.nika2003@gmail.com>
2023-04-08 19:05:25 +0300

    change hw2

--- homework_2.py
+++ homework_2.py
@@ -7,34 +7,25 @@ def main():
         if val_1 > val_2:
             print('Max value: ', val_1)
             return val_1
-        if val_1 < val_2:
-            print('Max value: ', val_2)
-            return val_2
-        if val_1 == val_2:
-            print('The numbers are equal to each other.\nMax value: ', val_1)
-            return val_1
-        return 0
+        print('Max value: ', val_2)
+        return val_2
 
-    def search_min(val_1, val_2):
-        if val_1 > val_2:
-            print('Min value: ', val_2)
-            return val_2
-        if val_1 < val_2:
+    def search_min(val_1, val_2, val_3):
+        if val_1 < val_2 and val_1 < val_3:
             print('Min value: ', val_1)
             return val_1
-        if val_1 == val_2:
-            print('The numbers are equal to each other.\nMin value: ', val_1)
-            return val_1
-        return 0
+        if val_2 < val_1 and val_2 < val_3:
+            print('Min value: ', val_2)
+            return val_2
+        print('Min value: ', val_3)
+        return val_3
 
     def search_abs(val):
         if val >= 0:
             print('Absolute value: ', val)
             return val
-        if val < 0:
-            print('Absolute value: ', -val)
-            return -val
-        return 0
+        print('Absolute value: ', -val)
+        return -val
 
     def print_sum(val_1, val_2):
         print(val_1 + val_2)
@@ -50,19 +41,20 @@ def main():
     print('Enter values to compare.')
     val_1 = int(input('Enter the first value: '))
     val_2 = int(input('Enter the second value: '))
+    val_3 = int(input('Enter the third value (for min): '))
     search_max(val_1, val_2)
-    search_min(val_1, val_2)
+    search_min(val_1, val_2, val_3)
 
-    val_3 = int(input('\nEnter the value to search for the abs: '))
-    search_abs(val_3)
+    val_4 = int(input('\nEnter the value to search for the abs: '))
+    search_abs(val_4)
 
     print('\nEnter values to search sum.')
-    val_4 = int(input('Enter the first value: '))
-    val_5 = int(input('Enter the second value: '))
-    print_sum(val_4, val_5)
+    val_5 = int(input('Enter the first value: '))
+    val_6 = int(input('Enter the second value: '))
+    print_sum(val_5, val_6)
 
-    val_6 = int(input('\nEnter a value to determine its value: '))
-    determ_pos_val(val_6)
+    val_7 = int(input('\nEnter a value to determine its value: '))
+    determ_pos_val(val_7)
 
 
 if __name__ == '__main__':
