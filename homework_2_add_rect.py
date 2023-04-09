29f8c1b4db9c87fefa1e10853c8f8a6088b3069f
Nikusion <bilima.nika2003@gmail.com>
2023-04-09 18:00:31 +0300

    *rename

--- homework_2_add_rect.py
+++ homework_2_add_rect.py
@@ -1,4 +1,4 @@
-def search_point_rect(vert_1, vert_3, point):
+def point_pos_rect(vert_1, vert_3, point):
     if(vert_1[0] <= point[0] <= vert_3[0] and
             vert_1[1] <= point[1] <= vert_3[1]):
         return True
@@ -14,7 +14,7 @@ vertice_3 = tuple(int(n) for n in vertice_3_input.split())
 point_input = input('Enter the coordinates of the point: ')
 point = tuple(int(n) for n in point_input.split())
 
-if search_point_rect(vertice_1, vertice_3, point):
+if point_pos_rect(vertice_1, vertice_3, point):
     print('Point enters the rectangle.')
 else:
     print('Point is outside the rectangle')
