10934ffc5514eca49da0a1077eb6749a55c0f8d3
Nikusion <bilima.nika2003@gmail.com>
2023-04-09 17:58:33 +0300

    *rename

--- homework_2_add_circle.py
+++ homework_2_add_circle.py
@@ -1,6 +1,6 @@
 import math
 
-def search_point_circle(center, radius, point):
+def point_pos_circle(center, radius, point):
     segment = math.sqrt((center[0]-point[0])**2+(center[1]-point[1])**2)
     if radius - segment > 0:
         print('Point enters the circle.')
@@ -18,4 +18,4 @@ radius = float(input('Enter the length of the radius: '))
 point_input = input('Enter the coordinates of the point: ')
 point = tuple(int(n) for n in point_input.split())
 
-search_point_circle(center, radius, point)
+point_pos_circle(center, radius, point)
