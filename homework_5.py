06c9dbd80c9b3f9911d38549664234183f75b194
Nikusion <bilima.nika2003@gmail.com>
2023-04-17 10:07:50 +0300

    hw5

--- homework_5.py
+++ homework_5.py
@@ -1,15 +1,15 @@
 txt = 'Hello, world!'
-print(txt[::-1])
+assert txt[::-1] == '!dlrow ,olleH'
+print('Reverse string: ', txt[::-1])
 
 txt_length = len(txt)
-print(txt_length)
+assert len(txt) == 13
 
 txt_list = list(txt)
-print(txt_list)
-print(txt_list.count('l'))
+assert txt_list == ['H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!']
 
-txt_2 = f"str {'|'.join((txt_list[::3]))}"
-print(txt_2)
+txt_2 = f"{'|'.join((txt_list[::3]))}"
+assert txt_2 == 'H|l| |r|!'
 
 
 def str_to_dict_1(string):
@@ -26,17 +26,20 @@ def str_to_dict_1(string):
             break
     return res_dict
 
-def count_str(str, k):
+
+def count_symbols(string, k):
     count = 0
-    for i in range(len(str)):
-        if(str[i] == k):
+    for i in range(len(string)):
+        if string[i] == k:
             count += 1
     return count
+
+
 def str_to_dict_2(string):
     keys = list(string)
     values = list()
     for val in keys:
-        amount = count_str(string, val)
+        amount = count_symbols(string, val)
         values.append(amount)
     res_dict = dict()
     for key in keys:
@@ -46,10 +49,14 @@ def str_to_dict_2(string):
             break
     return res_dict
 
-print(str_to_dict_1(txt))
-print(str_to_dict_2(txt))
+
+assert str_to_dict_1(txt) == {'H': 1, 'e': 1, 'l': 3, 'o': 2, ',': 1,
+                              ' ': 1, 'w': 1, 'r': 1, 'd': 1, '!': 1}
+assert str_to_dict_2(txt) == {'H': 1, 'e': 1, 'l': 3, 'o': 2, ',': 1,
+                              ' ': 1, 'w': 1, 'r': 1, 'd': 1, '!': 1}
 
 str_list = ['a', 'avcfdfdfdfd', 'sedefds']
+
 
 def search_longest_str(list_string):
     lengths = list()
@@ -62,10 +69,35 @@ def search_longest_str(list_string):
             res_dict[string] = len_s
             lengths.remove(len_s)
             break
-    print(res_dict)
-    res_dict_sort = sorted(res_dict.items(), key = lambda x: x[1], reverse = False)
-    return res_dict_sort[-1]
+    res_dict_sort = sorted(res_dict.items(), key=lambda k: k[1], reverse=False)
+    return res_dict_sort[-1][0]
 
 
-search_longest_str(str_list)
-print(search_longest_str(str_list)[0])
+print(f"\nThe longest str in {str_list}: {search_longest_str(str_list)}.")
+
+str_with_symbol = 'dfdf_asdea_asda_sdsd'
+
+
+def divide_and_glue(string, symbol):
+    list_string = string.split(symbol)
+    list_string = sorted(list_string)
+    new_string = symbol.join(list_string)
+    return new_string
+
+
+assert divide_and_glue(str_with_symbol, '_') == 'asda_asdea_dfdf_sdsd'
+
+numbers = [119, 101, 108, 108, 32, 100, 111, 110, 101]
+
+
+def ascii_codes(numbers):
+    numbers_ascii = list()
+    for num in numbers:
+        num_ascii = str(num).encode(encoding='ascii')
+        numbers_ascii.append(num_ascii)
+    numbers_ascii = ', '.join(str(i) for i in numbers_ascii)
+    print(f"\nString with ascii codes: \n{numbers_ascii}")
+    assert type(numbers_ascii) == str
+
+
+ascii_codes(numbers)
